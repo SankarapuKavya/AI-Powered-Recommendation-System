7dcb78d9a2590a6472284401e6bf6df8aa9ab96c
Kavya Sankarapu <kavya08092004@gmail.com>
2026-03-17 18:14:16 +0530

    Updated Settings

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+DATABASES = {
+    'default':
+    dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
